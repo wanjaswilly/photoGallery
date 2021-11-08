8a3c1e0b57b351092097f0ad33dd48acb20ffa96
sniper <wanjaswilly@gmail.com>
2021-11-08 22:24:52 +0300

    added files not included in last commit

---
+++ js/app.js
@@ -0,0 +1,53 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("upload");
+var span = document.getElementsByClassName("closeModal")[0];
+var closeButton = document.getElementsByClassName("closeModal")[1];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+closeButton.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+var fileobj;
+function upload_file(e) {
+    e.preventDefault();
+    fileobj = e.dataTransfer.files[0];
+    ajax_file_upload(fileobj);
+}
+  
+function file_explorer() {
+    document.getElementById('selectfile').click();
+    document.getElementById('selectfile').onchange = function() {
+        fileobj = document.getElementById('selectfile').files[0];
+        ajax_file_upload(fileobj);
+    };
+}
+  
+function ajax_file_upload(file_obj) {
+    if(file_obj != undefined) {
+        var form_data = new FormData();                  
+        form_data.append('file', file_obj);
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "kernel/engine.php", true);
+        xhttp.onload = function(event) {
+            oOutput = document.querySelector('.img-content');
+            if (xhttp.status == 200) {
+                oOutput.innerHTML = "<img src='"+ this.responseText +"' alt='The Image' />";
+            } else {
+                oOutput.innerHTML = "Error " + xhttp.status + " occurred when trying to upload your file.";
+            }
+        }
+ 
+        xhttp.send(form_data);
+    }
+}
